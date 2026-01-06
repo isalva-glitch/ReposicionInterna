a6d7b9dd088aa4bf46d0ad23f495953168a4b8f1
isalva-glitch <isalva@fontela.com.ar>
2026-01-06 15:07:40 -0300

    Mejorar log de inicio y manejo de errores

--- app/src/main/java/com/example/reposicioninterna/ReposicionInternaApp.kt
+++ app/src/main/java/com/example/reposicioninterna/ReposicionInternaApp.kt
@@ -5,6 +5,15 @@ import android.app.Application
 class ReposicionInternaApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        val previousHandler = Thread.getDefaultUncaughtExceptionHandler()
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+            StartupLog.log(
+                this,
+                "Fallo no controlado en hilo ${thread.name}",
+                throwable
+            )
+            previousHandler?.uncaughtException(thread, throwable)
+        }
         StartupLog.log(this, "Application onCreate")
     }
 }
